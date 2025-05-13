bd4f56b70e0ac04a078645e6138d79978d1fc980
Gaya3584 <gayugayus584@gmail.com>
2025-05-13 21:13:49 +0530

    handled submit

--- SingIt/src/body.jsx
+++ SingIt/src/body.jsx
@@ -1,9 +1,16 @@
 import './body.css';
-function Body() {
+import { useState } from 'react';
+function Body()
+{
+    const [lyrics, setLyrics] = useState('');
+    const handlesubmit=() => 
+    {
+        console.log("submitted Lyrics : ",lyrics);
+    };
     return (
         <div className="body">
-            <input type="text" className="input" placeholder="Enter Lyrics......" />
-            <button className="button">Submit</button>
+            <input type="text" className="input" placeholder="Enter Lyrics......" onChange={(e)=>setLyrics(e.target.value)}/>
+            <button className="button" onClick={handlesubmit}>Submit</button>
         </div>
     );
 }
